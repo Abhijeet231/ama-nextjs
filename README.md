547f6ceb9dc829a39912f832fbe67b101ee48e6e
Ramu kaka <pikupc01@gmail.com>
2025-11-28 10:50:54 +0530

    Add README and User model with Message schema

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-## This project is inspired by Hitesh Choudhary’s YouTube tutorial. I’m learning Next.js by building this AMA-style web application. It’s a personal fork/clone of the original ama-app, updated as I experiment and learn the Next.js framework.
+# AMA-App (Next.js)
+
+A Next.js AMA-style web application — built while learning Next.js by following a tutorial by Hitesh Choudhary.
+
+## 🚀 About
+
+This project is inspired by Hitesh Choudhary’s YouTube tutorial. I’m using this app to learn Next.js concepts and build a real-world web application at the same time. It’s a personal fork/clone of the original “ama-app,” and I’ll be updating and extending it as I progress.
+
+## 🧰 Tech Stack
+
+- Next.js  
+- React  
+- tailwind css
+- mongoDB
+-  ShadCN
+- Zod
+- React Hook Form
+- Next Auth/ Auth.js
+
+
+
---
+++ src/model/User.ts
@@ -0,0 +1,83 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+// export interface Message extends Document {
+//     content: string;
+//     createdAt: Date
+// }
+
+// ** MESSAGE SCHEMA **
+
+// Modern Syntax
+export interface Message {
+    content: string;
+    createdAt: Date;
+}
+
+  const MessageSchema = new Schema<Message> ({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+
+    }
+        
+  })
+
+  // ** USER SCHEMA ** 
+
+
+  export interface User {
+    userName: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[];
+  }
+
+  const UserSchema = new Schema<User> ({
+    userName: {
+        type: String ,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/ , "please use a valid email address"]
+    },
+    password: {
+        type: String,
+        required: [true, "password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "VerifyCode is required"],
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, 'VerifyCode expiry is required'],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true,
+    },
+    messages: [MessageSchema]
+
+})
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel;
